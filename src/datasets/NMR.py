God help me. Fixing different normalization strategies for encoder and decoder. Adding sampling procedure to the denoiser. Fixing minor bugs.

src/datasets/NMR.py
@@ -24,10 +24,11 @@ from ..utils import misc_utils
 from .camera_utils import compute_ray_grids, compute_ray_grids_for_views, check_ray_grid_with_pointcloud
 
 class ObjectDataset(Dataset):
-    def __init__(self, obj_paths, class_ids, img_size = (32, 32), transform=None, num_views=2) -> None:
+    def __init__(self, obj_paths, class_ids, img_size = (32, 32), source_transform=None, target_transform=None, num_views=2) -> None:
         self.obj_paths = obj_paths
         self.img_size = img_size
-        self.transform = transform
+        self.source_transform = source_transform
+        self.target_transform = target_transform
         self.num_views = num_views
         self.class_ids = class_ids
         
@@ -67,8 +68,10 @@ class ObjectDataset(Dataset):
         views = [decode_image(obj_path.joinpath('image').joinpath(vfn)) for vfn in view_files_names]
 
         # Apply transformations if provided
-        if self.transform:
-            views = torch.stack([self.transform(view) for view in views], dim=0)
+        if self.source_transform and self.target_transform:
+            views = [self.source_transform(view) for view in views[:-1]] + [self.target_transform(views[-1])]
+            views = torch.stack(views, dim=0)
+            
             
         ray_grids = compute_ray_grids_for_views(obj_path.joinpath('cameras.npz'), H=self.img_size[0],
                                                 W=self.img_size[1], views_idx=view_idxs, use_canonical=True)
@@ -80,7 +83,7 @@ class ObjectDataset(Dataset):
 
         return views, ray_grids, label
 
-    def get_all_views(self, idx):
+    def get_all_views(self, idx, transform:str='src'):
         obj_path = self.obj_paths[idx]
         
         parts = obj_path.parts
@@ -92,8 +95,10 @@ class ObjectDataset(Dataset):
         views = [decode_image(obj_path.joinpath('image').joinpath(vfn)) for vfn in view_files_names]
         
         # Apply transformations if provided
-        if self.transform:
-            views = torch.stack([self.transform(view) for view in views], dim=0)
+        if transform == 'src' and self.source_transform:
+            views = torch.stack([self.source_transform(view) for view in views], dim=0)
+        elif transform == 'trgt' and self.target_transform:
+            views = torch.stack([self.target_transform(view) for view in views], dim=0)
             
         ray_grids = compute_ray_grids(obj_path.joinpath('cameras.npz'), H=self.img_size[0], W=self.img_size[1], use_canonical=True)
         

src/models/SODA.py
@@ -16,7 +16,7 @@ class SODA(nn.Module):
                  enc_img_shape:tuple = (3, 32, 32),
                  z_dim:int = 128,
                  c_dim:int = None,
-                 c_pos_emb_freq:int = 10,
+                 c_pos_emb_freq:int = 6,
                  
                  dec_img_shape:tuple = (3, 32, 32),
                  dec_dropout:float = 0.1,
@@ -25,7 +25,8 @@ class SODA(nn.Module):
                  beta_schedule:str = 'inverted',
                  T:int = 1000,
                  t_dim:int = 512,
-                 condition_drop_prob:float = 0.1
+                 z_drop_prob:float = 0.12,
+                #  c_drop_prob:float = 0.1,
                  )->None:
         ''' SODA proposed by "SODA: Bottleneck Diffusion Models for Representation Learning", and \
             DDPM proposed by "Denoising Diffusion Probabilistic Models", as well as \
@@ -60,14 +61,19 @@ class SODA(nn.Module):
                             t_dim=t_dim,
                             z_dim=z_dim,
                             c_dim=c_dim,
+                            c_pos_emb_freq=c_pos_emb_freq,
                             self_attention_type=self_att_type
                             )
+        
+        self.enc_img_shape = enc_img_shape
+        self.dec_img_shape = dec_img_shape
         
         self.ddpm_sche = get_schedule(beta_schedule, T, 'DDPM')
         self.ddim_sche = get_schedule(beta_schedule, T, 'DDIM')
         
         self.T = T
-        self.condition_drop_prob = condition_drop_prob
+        self.z_drop_prob = z_drop_prob
+        self.c_drop_prob = c_drop_prob
         self.loss = nn.MSELoss()
         
         self.device = torch.device('cpu')
@@ -114,11 +120,15 @@ class SODA(nn.Module):
         
     def training_step(self, x_source, x_target, c_source=None, c_target=None, use_amp=False):
         x_recon, noise = self(x_source, x_target, c_source, c_target, use_amp)
-        return self.loss(noise, x_recon)
+        with autocast('cuda', enabled=use_amp):
+            loss = self.loss(noise, x_recon)
+        return loss
     
     def validation_step(self, x_source, x_target, c_source=None, c_target=None, use_amp=False):
         x_recon, noise = self(x_source, x_target, c_source, c_target, use_amp)
-        return self.loss(noise, x_recon)
+        with autocast('cuda', enabled=use_amp):
+            loss = self.loss(noise, x_recon)
+        return loss
         
     def forward(self, x_source, x_target, c_source=None, c_target=None, use_amp=False):
         ''' Training with simple noise prediction loss.
@@ -135,7 +145,7 @@ class SODA(nn.Module):
         x_noised, t, noise = self.perturb(x_target, t=None)
 
         # 0 for conditional, 1 for unconditional
-        mask = torch.bernoulli(torch.zeros(x_noised.shape[0]) + self.condition_drop_prob).to(self.device)
+        mask = torch.bernoulli(torch.zeros(x_noised.shape[0]) + self.z_drop_prob).to(self.device)
         with autocast('cuda', enabled=use_amp):
             if self.use_encoder:
                 z = self.encoder(x_source, c_source)
@@ -152,12 +162,11 @@ class SODA(nn.Module):
         return z
     
     
-    def ddim_sample(self, n_sample, size, z_guide, c_cond, steps=100, eta=0.0, guide_w=0.3, notqdm=False, use_amp=False):
+    def ddim_sample(self, n_sample, z_guide, c_cond, steps=100, eta=0.0, guide_w=0.3, notqdm=False, use_amp=False):
         ''' Sampling with DDIM sampler. Actual NFE is `2 * steps`.
 
             Args:
                 n_sample: The batch size.
-                size: The image shape (e.g. `(3, 32, 32)`).
                 z_guide: The latent code extracted from real images (for guidance).
                 c_cond: Conditioning tensor for conditional generation.
                 steps: The number of total timesteps.
@@ -166,22 +175,23 @@ class SODA(nn.Module):
             Returns:
                 The sampled image tensor ranged in `[0, 1]`.
         '''
+        size = self.dec_img_shape
         sche = self.ddim_sche
-        model_args = self.prepare_condition_(n_sample, z_guide)
+        z_guide, mask = self.prepare_condition_(n_sample, z_guide)
         x_i = torch.randn(n_sample, *size).to(self.device)
 
-        times = torch.arange(0, self.n_T, self.n_T // steps) + 1
+        times = torch.arange(0, self.T, self.T // steps) + 1
         times = list(reversed(times.int().tolist())) + [0]
         time_pairs = list(zip(times[:-1], times[1:]))
         # e.g. [(801, 601), (601, 401), (401, 201), (201, 1), (1, 0)]
 
-        for time, time_next in tqdm(time_pairs, disable=notqdm):
-            t_is = torch.tensor([time / self.n_T]).to(self.device).repeat(n_sample)
+        for time, time_next in tqdm(time_pairs, disable=notqdm, desc="Sampling using DDIM"):
+            t_is = torch.tensor([time / self.T]).to(self.device).repeat(n_sample)
 
             z = torch.randn(n_sample, *size).to(self.device) if time_next > 0 else 0
 
             alpha = sche["alphabar_t"][time]
-            eps, x0_t = self.pred_eps_(x_i, t_is, model_args, guide_w, alpha, use_amp)
+            eps, x0_t = self.pred_eps_(x_i, t_is, z_guide, mask, c_cond, guide_w, alpha, use_amp)
             alpha_next = sche["alphabar_t"][time_next]
             c1 = eta * ((1 - alpha / alpha_next) * (1 - alpha_next) / (1 - alpha)).sqrt()
             c2 = (1 - alpha_next - c1 ** 2).sqrt()
@@ -190,14 +200,14 @@ class SODA(nn.Module):
         return x_i
     
     
-    def pred_eps_(self, x, t, model_args, guide_w, alpha, use_amp, clip_x=True):
+    def pred_eps_(self, x, t, z, mask, c, guide_w, alpha, use_amp, clip_x=True):
         def pred_cfg_eps_double_batch():
             # double batch
             x_double = x.repeat(2, 1, 1, 1)
             t_double = t.repeat(2)
 
             with autocast('cuda', enabled=use_amp):
-                eps = self.decoder(x_double, t_double, *model_args).float()
+                eps = self.decoder(x_double, t_double, z, mask, c).float()
             n_sample = eps.shape[0] // 2
             eps1 = eps[:n_sample]
             eps2 = eps[n_sample:]

src/models/blocks.py
@@ -20,7 +20,7 @@ class Upsample(nn.Module):
     def __init__(self, n_channels, use_conv=True):
         super().__init__()
         self.use_conv = use_conv
-        self.up = nn.Upsample(scale_factor = 2, mode = 'nearest'),
+        self.up = nn.Upsample(scale_factor = 2, mode = 'nearest')
         if use_conv:
             self.conv = nn.Conv2d(n_channels, n_channels, kernel_size=3, stride=1, padding=1)
 
@@ -201,7 +201,7 @@ class TimeEmbedding(nn.Module):
     def forward(self, t):
         # Create sinusoidal position embeddings (same as those from the transformer)
         half_dim = self.n_channels // 8
-        emb = torch.log(10_000) / (half_dim - 1)
+        emb = torch.log(torch.tensor(10_000)) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=t.device) * -emb)
         emb = t.float()[:, None] * emb[None, :]
         emb = torch.cat((emb.sin(), emb.cos()), dim=1)
@@ -281,7 +281,7 @@ class PoseEmbedding(nn.Module):
         D = orig_shape[-1]  # last dimension is the coordinate dimension
 
         # 2) Flatten everything except the last dimension
-        x_flat = x.view(-1, D)  # shape (N, D)
+        x_flat = x.reshape(-1, D)  # shape (N, D)
         n = x_flat.shape[0]     # number of flattened elements
 
         # 3) Create frequency multipliers [1, 2, 4, ..., 2^(num_freqs-1)]

src/models/soda_encoder.py
@@ -12,7 +12,7 @@ class SodaEncoder(nn.Module):
                  img_shape:tuple = (3, 32, 32),
                  z_dim:int = 128,
                  c_dim:int = None,
-                 c_pos_emb_freq:int = 10,
+                 c_pos_emb_freq:int = 6,
                  ) -> None :
         
         

src/models/unet.py
@@ -10,7 +10,7 @@ from .blocks import DownsampleResBlock, UpsampleResBlock, ResAttBlock, MiddleBlo
 
 class UNet(nn.Module):
     def __init__(self, 
-                 img_shape = [3, 64, 64], 
+                 img_shape = (3, 32, 32), 
                  n_channels = 128,
                  ch_mults = (1, 2, 2, 2),
                  is_attn = (False, True, False, False),
@@ -21,6 +21,7 @@ class UNet(nn.Module):
                  t_dim = 512,
                  z_dim = 128,
                  c_dim = None,
+                 c_pos_emb_freq:int = 6,
                  self_attention_type = 'nromal'
                  ):
         """
@@ -42,7 +43,7 @@ class UNet(nn.Module):
         
         
         if c_dim:
-            self.c_emb = PoseEmbedding(num_freqs=10)
+            self.c_emb = PoseEmbedding(num_freqs=c_pos_emb_freq)
             # Linear
             self.rgb_linear_projector = nn.Conv2d(img_shape[0], n_channels, kernel_size=1)
             self.image_proj = nn.Conv2d(n_channels + c_dim, n_channels, kernel_size=3, padding=1)
@@ -109,9 +110,9 @@ class UNet(nn.Module):
         self.final = nn.Conv2d(out_channels, img_shape[0], kernel_size=3, padding=1)
         
         
-    def forward(self, x, t, z, drop_mask, grid_condition=None, ret_activation=False):
+    def forward(self, x, t, z, drop_mask, c=None, ret_activation=False):
         if not ret_activation:
-            return self.forward_core(x, t, z, drop_mask)
+            return self.forward_core(x, t, z, drop_mask, c)
 
         activation = {}
         def namedHook(name):
@@ -126,7 +127,7 @@ class UNet(nn.Module):
                 name = f'out_{no}'
                 hooks[name] = blk.register_forward_hook(namedHook(name))
 
-        result = self.forward_core(x, t, z, drop_mask, grid_condition)
+        result = self.forward_core(x, t, z, drop_mask, c)
         for name in hooks:
             hooks[name].remove()
         return result, activation
@@ -143,7 +144,7 @@ class UNet(nn.Module):
         t = self.time_emb(t)
         z = self.z_emb(z, drop_mask)
         if c is not None:
-            c = self.grid_cond_emb(c)
+            c = self.c_emb(c)
             c = c.permute(0, 3, 1, 2)
             x = self.rgb_linear_projector(x)
             x = self.image_proj(torch.cat((x, c), dim=1))